c57e9a0340bddd04d20242309c8f27c07a4b3d03
krokuta <marynako@interia.pl>
2019-10-15 13:54:27 +0200

    module 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,45 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println();
+        System.out.println();
+
+        //Zadanie 6.2
+        //test jednostkowy 1
+        System.out.println("test calculator - dodawanie:");
+
+        Calculator calculator = new Calculator(7.4, 3.9);
+
+        if(calculator.adding() == 7.4 + 3.9){
+            System.out.println("works");
+        } else {
+            System.out.println("nope...");
+        }
+        System.out.println();
+
+        //Zadanie 6.2
+        //test jednostkowy 2
+        System.out.println("test calculator - odejmowanie:");
+
+        if(calculator.subtracting() == 7.4 - 3.9){
+            System.out.println("works");
+        } else {
+            System.out.println("nope...");
+        }
+        System.out.println();
     }
-}
+
+    }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    double a;
+    double b;
+
+    public Calculator(double a, double b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public double adding(){
+        double add;
+        add = a + b;
+        return add;
+    }
+
+    public double subtracting(){
+        double sub;
+        sub = a - b;
+        return sub;
+    }
+
+    public double getA(){
+        return a;
+    }
+
+    public double getB(){
+        return b;
+    }
+}
