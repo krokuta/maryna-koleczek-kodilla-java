8b027fe4474d964193fe1a8b89f7c2b63363024e
krokuta <marynako@interia.pl>
2019-10-31 14:12:37 +0100

    7.5.2

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,4 +1,14 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
+    static double getAverage(int[] numbers){
+        IntStream.rangeClosed(1, 10)
+                .forEach(System.out::println);
+        OptionalDouble avg = IntStream.rangeClosed(1, 10)
+                .average();
+        return avg.getAsDouble();
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,18 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.OptionalDouble;
+
 public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //When
+        double returnedAverage = ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(5.5, returnedAverage, 0.001);
+    }
 }
